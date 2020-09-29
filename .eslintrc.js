fcb59cba395feda4fbe6eb311300dbb5e45b65fa
省局到 <3107075636@qq.com>
2020-09-29 16:36:44 +0800

    feat: lint-rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
+/* eslint-disable */
 module.exports = {
-  root: true,
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 7,
+    "sourceType": "module"
+  },
   rules: {
-    'no-console': 'off',
+    'no-console': 'error',
     'no-debugger': 'off'
   }
 }
+/* eslint-disable */
