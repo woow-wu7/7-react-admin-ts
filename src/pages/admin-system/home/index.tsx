2dae98e640850e18a25ac6ed25481765e4eb0472
省局到 <3107075636@qq.com>
2020-10-10 22:14:30 +0800

    feat: echarts bar resize

--- src/pages/admin-system/home/index.tsx
+++ src/pages/admin-system/home/index.tsx
@@ -1,11 +1,37 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import echarts from 'echarts'
+import styles from './home.module.scss'
 
 const Home = (props: any) => {
-  const bar = useRef(null)
+  const bar1: any = useRef(null)
+
+  useEffect(() => {
+    const barChart = echarts.init(bar1.current)
+    window.addEventListener('resize', function () {
+      barChart.resize()
+    })
+    const option = {
+      title: { text: 'ECharts 入门示例' },
+      tooltip: {},
+      xAxis: {
+        data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+    barChart.setOption(option);
+  }, [bar1])
+
   return (
-    <div>
-      <div ref={bar}/>
+    <div className={styles.home}>
+      <div className={styles.top}>
+        <div ref={bar1} className={styles.bar} />
+        <div className={styles.bar} />
+      </div>
     </div>
   )
 }
