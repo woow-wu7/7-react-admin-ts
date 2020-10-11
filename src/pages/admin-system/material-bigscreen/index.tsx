4f352b2bf83274090a1ba636bac87cb079ad90ea
省局到 <3107075636@qq.com>
2020-10-11 11:33:44 +0800

    feat: echarts

--- src/pages/admin-system/material-bigscreen/index.tsx
+++ src/pages/admin-system/material-bigscreen/index.tsx
@@ -21,6 +21,14 @@ const Material = (props: any) => {
 
         <a href="https://github.com/sindresorhus/screenfull.js" target="_blank">(1) screenfull.js</a>
       </div>
+
+      <div className={styles.plugins}>
+        <p className={styles.title}>教程</p>
+
+        <a href="https://segmentfault.com/a/1190000020445644" target="_blank">(1) react中 echarts的封装</a>
+        <a href="https://www.cnblogs.com/zhangnan35/p/12680038.html" target="_blank">(2) vue中 echarts的封装</a>
+
+      </div>
     </div>
   )
 }
