aca2372973622a9fa26c0cdfad3ea0545a9d1720
省局到 <3107075636@qq.com>
2020-10-16 15:21:30 +0800

    feat: optimize useFetch

--- src/utils/hooks/use-fetch.ts
+++ src/utils/hooks/use-fetch.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 
 interface IFetch {
   (params?: any): any;
@@ -8,12 +8,20 @@ interface IFetch {
  * @function useFetch 请求hooks
  * @param {function} fetchFn 请求函数
  * @param {object} fnParams 请求函数的参数
+ * @param {function} converter 原始数据的转换函数，默认为恒等函数，用于过滤等
  */
-export function useFetch(fetchFn: IFetch, fnParams = {}) {
+export function useFetch(
+  fetchFn: IFetch,
+  fnParams = {},
+  converter: IFetch = data => data
+) {
   const [data, setData] = useState([] || {})
   const [params, setParPms] = useState(fnParams)
   const [isError, setIsError] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+
+  // useEffect当依赖数组中有对象，函数等引用类型时，必须做函数缓存保证每次Object.is判断是true，不会造成死循环
+  const memoryConverter = useCallback(converter, [])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,17 +30,17 @@ export function useFetch(fetchFn: IFetch, fnParams = {}) {
       try {
         const res = await fetchFn(params)
         if (res.data) {
-          setData(data => data = res.data)
+          setData(data => data = memoryConverter(res.data))
           setIsLoading(false)
         }
-      } catch(err) {
+      } catch (err) {
         setIsError(true)
       }
       setIsLoading(false)
     }
 
     fetchData()
-  }, [params])
+  }, [params, memoryConverter, fetchFn])
 
   const doFetch = (params: any) => {
     setParPms(params)
