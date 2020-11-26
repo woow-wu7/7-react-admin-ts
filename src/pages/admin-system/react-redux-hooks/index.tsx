08ddba3eabac82b94558e5210877984b01ef6679
woow_wu7 <3107075636@qq.com>
2020-11-26 13:43:03 +0800

    feat: link

--- src/pages/admin-system/react-redux-hooks/index.tsx
+++ src/pages/admin-system/react-redux-hooks/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import actionType from './constant'
 
 const equalityFn = (prevState: any, nextState: any) => {
@@ -25,6 +25,14 @@ const ReactReduxHooks = () => {
       <p>react-redux-hooks</p>
       <button onClick={addNumber}>点击add，验证react-redux中的useSelector的第二个参数equalityFn返回的布尔值</button>
       <div>{number}</div>
+      <div style={{ margin: '20px 0', border: '1px solid olive', padding: '0 10px' }}>
+        <br />
+        <a href="https://github.com/woow-wu7/7-react-admin-ts/tree/master/src/pages/admin-system/react-redux-hooks" target="__blank">react-redux-hooks -- README.md</a>
+        <br /><br />
+        <a href="https://react-redux.js.org/introduction/quick-start" target="__blacnk">react-redux官网</a>
+        <br />
+        <br />
+      </div>
     </div>
   )
 }
