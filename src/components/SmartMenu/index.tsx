67bd98941510b41a627de987c08e06ac0ee419cf
woow_wu7 <3107075636@qq.com>
2020-11-28 16:26:12 +0800

    refactor: menu openkeys selectedkeys persistent

--- src/components/SmartMenu/index.tsx
+++ src/components/SmartMenu/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IRouteModule } from '@/global/interface'
 import { routesFilter } from '@/utils/render-routes/index'
 import { Menu } from 'antd';
 import { useSelector } from 'react-redux';
 import { CONST } from '@/global/enum';
 import IconFont from '@/components/Icon-font'
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { getLocalStorage, setLocalStorage } from '@/utils';
 import adminRoutes from '@/router/admin-routes'
 
@@ -14,8 +14,25 @@ const { SubMenu } = Menu;
 
 const SmartMenu = () => {
   const history = useHistory()
+  const location = useLocation()
   const [selectedKeys, setSelectedKeys] = useState(['/admin-home'])
   const [openKeys, setOpenKeys]: any = useState(['/admin-home'])
+
+  useEffect(() => {
+    // 说明：
+    // (1)
+    // 这里要考虑 ( 登陆第一次跳转的加载 ) 和 ( 刷新浏览器的加载 )
+    // 不管哪种情况，都获取当前的 pathname，当前pathname是 ( path和menu的keys要一致的原因  )
+    // (2)
+    // openKeys ===> 是要存在localStorage的，因为需要持久化保存，刷新按钮刷新时，获取
+    // selectkeys => 不需要做持久化，而是从 location.pathname 中获取，因为我们的routes中 path 和 key 是一致的，获取的当前path就是当前选中的menu.item的key
+    // (3)
+    // 该 effect 同时用于初始化和刷新
+    const CurrentopenKeys = getLocalStorage(CONST.OPENKEYS) || []
+    const currentPathname = location.pathname
+    setOpenKeys(() => CurrentopenKeys) // 菜单的展开和折叠的 keys，每一个menu成员都有一个唯一的key
+    setSelectedKeys(() => [currentPathname]) // 选中菜单的keys数组，我们的 route 中的  key 和 path 是一样的
+  }, [location.pathname])
 
   const roles =
     useSelector((state: { app: { loginMessage: { roles: string } } }) => state.app.loginMessage.roles) ||
