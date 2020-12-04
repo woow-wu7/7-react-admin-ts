440513b7d81a20ea32ba73b4eb1ea8fe43ff1675
woow_wu7 <3107075636@qq.com>
2020-12-04 23:16:25 +0800

    style: supplement

---
+++ src/pages/admin-system/interview-react/Knowledge/StickyFooter/index.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import StickyFooterMarginPadding from './Marginpadding'
+import StickyFooterFlex from './Flex'
+import StickyFooterCalc from './Calc'
+
+import './sticky-footer.scss'
+
+const StickyFooter = () => {
+  const [links] = useState([
+    {
+      name: 'stickyFooter三种方法 - 我的掘金博客',
+      url: 'https://juejin.cn/post/6844904090644774926'
+    },
+  ])
+  const renderLinks = () => links.map(({ name, url }) => <div key={name}><a href={url} target="blank">{name}</a></div>)
+
+  return (
+    <div className="sticky-footer">
+      <p>StickyFooter 效果</p>
+      <div>
+        <StickyFooterMarginPadding />
+        <StickyFooterFlex />
+        <StickyFooterCalc />
+      </div>
+
+      <div>
+        {renderLinks()}
+      </div>
+    </div>
+  )
+}
+
+export default StickyFooter
