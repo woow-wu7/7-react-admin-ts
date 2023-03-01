842b45913f32d36924ce7a966aeb389fdd9d4377
woow_wu7 <woow.wu7@gmail.com>
2023-03-01 15:16:35 +0800

    feat: koa.

--- src/SOURCE-CODE-ANALYSIS/KOA/lib/application.js
+++ src/SOURCE-CODE-ANALYSIS/KOA/lib/application.js
@@ -1,32 +1,80 @@
-'use strict'
+"use strict";
 
 /**
  * Module dependencies.
  */
 
-const debug = require('debug')('koa:application')
-const onFinished = require('on-finished')
-const response = require('./response')
+const debug = require("debug")("koa:application");
+const onFinished = require("on-finished");
+const response = require("./response");
 
-const compose = require('koa-compose')
+const compose = require("koa-compose");
 // koa-compose
 // 仓库地址：https://github.com/Cool-Primavera/compose/blob/master/index.js
 // 本项目已经将koa-compose拷贝到koa-compose.js中
 
-const context = require('./context')
-const request = require('./request')
-const statuses = require('statuses')
-const Emitter = require('events')
-const util = require('util')
-const Stream = require('stream')
-const http = require('http')
-const only = require('only')
-const { HttpError } = require('http-errors')
+const context = require("./context");
+const request = require("./request");
+const statuses = require("statuses");
+const Emitter = require("events");
+const util = require("util");
+const Stream = require("stream");
+const http = require("http");
+const only = require("only");
+const { HttpError } = require("http-errors");
 
 /**
  * Expose `Application` class.
  * Inherits from `Emitter.prototype`.
  */
+
+// 1
+// 使用案例
+// const Koa = require("./lib/application.js");
+// const app = new Koa();
+// app.use(async (ctx, next) => { console.log(1); await next(); console.log(2); });
+// app.use(async (ctx, next) => { console.log(3); await next(); console.log(4); });
+// app.use(async (ctx, next) => { console.log(5); ctx.body = "测试中间执行顺序"; });
+// app.listen(1000, () => 'app run 1000')
+
+// 2
+// KOA 整个入口文件
+// - 整个顺序: app.listen() ---> callback() ---> handleRequest() ---> 中间件fn(ctx).then(handleResponse).catch(onerror)
+// - fnMiddleware = compose(this.middleware) = function (context, next) =>  dispatch(0)
+// ====== 三个中间价
+// app.use(async (ctx, next) => {
+//   console.log(1);
+//   await next();
+//   console.log(2);
+// });
+// app.use(async (ctx, next) => {
+//   console.log(3);
+//   await next();
+//   console.log(4);
+// });
+// app.use(async (ctx, next) => {
+//   console.log(5);
+//   ctx.body = "测试中间执行顺序";
+// });
+// ===== 洋葱模型
+// fnMiddleware() => Promise.resolve(
+//   // fn1()
+//   console.log(1)
+//   await Promise.resolve( // ------ next()
+//     // fn2()
+//     console.log(3)
+//     await Promise.resolve( // ---- next()
+//       // fn3()
+//       console.log(5)
+//       return Promise.resolve()
+//     )
+//     console.log(4)
+//   )
+//   console.log(2)
+// )
+// .then(handleResponse)
+// .catch(onerror)
+// 13542
 
 module.exports = class Application extends Emitter {
   /**
@@ -48,22 +96,22 @@ module.exports = class Application extends Emitter {
    */
 
   constructor(options) {
-    super()
-    options = options || {}
-    this.proxy = options.proxy || false
-    this.subdomainOffset = options.subdomainOffset || 2
-    this.proxyIpHeader = options.proxyIpHeader || 'X-Forwarded-For'
-    this.maxIpsCount = options.maxIpsCount || 0
-    this.env = options.env || process.env.NODE_ENV || 'development'
-    if (options.keys) this.keys = options.keys
-    this.middleware = []
-    this.context = Object.create(context)
-    this.request = Object.create(request)
-    this.response = Object.create(response)
+    super();
+    options = options || {};
+    this.proxy = options.proxy || false;
+    this.subdomainOffset = options.subdomainOffset || 2;
+    this.proxyIpHeader = options.proxyIpHeader || "X-Forwarded-For";
+    this.maxIpsCount = options.maxIpsCount || 0;
+    this.env = options.env || process.env.NODE_ENV || "development";
+    if (options.keys) this.keys = options.keys;
+    this.middleware = [];
+    this.context = Object.create(context);
+    this.request = Object.create(request);
+    this.response = Object.create(response);
     // util.inspect.custom support for node 6+
     /* istanbul ignore else */
     if (util.inspect.custom) {
-      this[util.inspect.custom] = this.inspect
+      this[util.inspect.custom] = this.inspect;
     }
   }
 
@@ -77,12 +125,50 @@ module.exports = class Application extends Emitter {
    * @api public
    */
 
-  // ------------------------------------------------------------------------------------------------------------------- listen
+  // ----------------------------------------------------------------------------------------------- listen
   // app.listen(3000)
+  // app.listen(1000, () => 'app run 1000')
   listen(...args) {
-    debug('listen')
-    const server = http.createServer(this.callback())
-    return server.listen(...args)
+    debug("listen");
+
+    const server = http.createServer(this.callback());
+    // 1 this.callback()
+    // - 调用后，返回的是下面这样的 函数
+    // const handleRequest = (req, res) => {
+    //   const ctx = this.createContext(req, res); // 创建context对象，包含request，response，req，res等属性
+    //   return this.handleRequest(ctx, compose(this.middleware));
+    // };
+
+    // 2
+    // compose(this.middleware)
+    // - 调用后，返回的函数最终形态
+    // 6. 最终形态如下
+    //   const [fn1, fn2, fn3] = this.middleware()
+    //   const fnMiddleware = function(context, next) {
+    //     return Promise.resolve(fn1(context, function next1() {
+    //       return Promise.resolve(fn2(context, function next2() { // 每个中间件，如果存在next就return Promise.resolve(fn(context, dispatch.bind(null, i + 1)))，不存在就 return Promise.resolve()
+    //         return Promise.resolve(fn3(context, function next3() { // 最后一个中间件没有next函数了，因为已经是最后一个
+    //           return Promise.resolve()
+    //         }))
+    //       }))
+    //     }))
+    //   }
+
+    // 3
+    // http.createServer是 ( node原生api )
+    // http.createServer([options][, requestListener])
+    // http.createServer
+    // 使用如下
+    // const http = require('node:http');
+    // const server = http.createServer((req, res) => {
+    //   res.writeHead(200, { 'Content-Type': 'application/json' });
+    //   res.end(JSON.stringify({
+    //     data: 'Hello World!'
+    //   }));
+    // });
+    // server.listen(8000);
+
+    return server.listen(...args);
   }
 
   /**
@@ -94,7 +180,7 @@ module.exports = class Application extends Emitter {
    */
 
   toJSON() {
-    return only(this, ['subdomainOffset', 'proxy', 'env'])
+    return only(this, ["subdomainOffset", "proxy", "env"]);
   }
 
   /**
@@ -105,7 +191,7 @@ module.exports = class Application extends Emitter {
    */
 
   inspect() {
-    return this.toJSON()
+    return this.toJSON();
   }
 
   /**
@@ -117,7 +203,7 @@ module.exports = class Application extends Emitter {
    * @return {Application} self
    * @api public
    */
-  // ------------------------------------------------------------------------------------------------------------------- use
+  // ----------------------------------------------------------------------------------------------- use
   // use -> app.use
   // 1
   /*
@@ -127,10 +213,11 @@ module.exports = class Application extends Emitter {
     app.listen(3000);
   */
   use(fn) {
-    if (typeof fn !== 'function') throw new TypeError('middleware must be a function!') // 中间价必须是函数
-    debug('use %s', fn._name || fn.name || '-')
-    this.middleware.push(fn) // push -> fn
-    return this // this是use方法调用时所在的对象，通过app.use()调用，即this指向实例app -> 返回this，链式调用
+    if (typeof fn !== "function")
+      throw new TypeError("middleware must be a function!"); // 中间价必须是函数
+    debug("use %s", fn._name || fn.name || "-");
+    this.middleware.push(fn); // push -> fn
+    return this; // this是use方法调用时所在的对象，通过app.use()调用，即this指向实例app -> 返回this，链式调用
   }
 
   /**
@@ -141,18 +228,24 @@ module.exports = class Application extends Emitter {
    * @api public
    */
 
-  // ------------------------------------------------------------------------------------------------------------------- callback
+  // ----------------------------------------------------------------------------------------------- callback
+  // listen(...args) {
+  //   debug('listen')
+  //   const server = http.createServer(this.callback()) ----- callback
+  //   return server.listen(...args)
+  // }
   callback() {
-    const fn = compose(this.middleware)
+    // middleware 是 app.use(fn) 中 push 的 fn，即 middleware(fn1, fn2, fn3)
+    const fn = compose(this.middleware);
 
-    if (!this.listenerCount('error')) this.on('error', this.onerror)
+    if (!this.listenerCount("error")) this.on("error", this.onerror);
 
     const handleRequest = (req, res) => {
-      const ctx = this.createContext(req, res)
-      return this.handleRequest(ctx, fn)
-    }
+      const ctx = this.createContext(req, res); // 创建context对象，包含request，response，req，res等属性
+      return this.handleRequest(ctx, fn);
+    };
 
-    return handleRequest
+    return handleRequest;
   }
 
   /**
@@ -162,12 +255,12 @@ module.exports = class Application extends Emitter {
    */
 
   handleRequest(ctx, fnMiddleware) {
-    const res = ctx.res
-    res.statusCode = 404
-    const onerror = (err) => ctx.onerror(err)
-    const handleResponse = () => respond(ctx)
-    onFinished(res, onerror)
-    return fnMiddleware(ctx).then(handleResponse).catch(onerror)
+    const res = ctx.res;
+    res.statusCode = 404;
+    const onerror = (err) => ctx.onerror(err);
+    const handleResponse = () => respond(ctx);
+    onFinished(res, onerror);
+    return fnMiddleware(ctx).then(handleResponse).catch(onerror);
   }
 
   /**
@@ -175,20 +268,21 @@ module.exports = class Application extends Emitter {
    *
    * @api private
    */
-
+  // ----------------------------------------------------------------------------------------------- createContext
+  // 创建context对象，包含request，response，req，res等属性
   createContext(req, res) {
-    const context = Object.create(this.context)
-    const request = (context.request = Object.create(this.request))
-    const response = (context.response = Object.create(this.response))
-    context.app = request.app = response.app = this
-    context.req = request.req = response.req = req
-    context.res = request.res = response.res = res
-    request.ctx = response.ctx = context
-    request.response = response
-    response.request = request
-    context.originalUrl = request.originalUrl = req.url
-    context.state = {}
-    return context
+    const context = Object.create(this.context);
+    const request = (context.request = Object.create(this.request));
+    const response = (context.response = Object.create(this.response));
+    context.app = request.app = response.app = this;
+    context.req = request.req = response.req = req;
+    context.res = request.res = response.res = res;
+    request.ctx = response.ctx = context;
+    request.response = response;
+    response.request = request;
+    context.originalUrl = request.originalUrl = req.url;
+    context.state = {};
+    return context;
   }
 
   /**
@@ -202,14 +296,17 @@ module.exports = class Application extends Emitter {
     // When dealing with cross-globals a normal `instanceof` check doesn't work properly.
     // See https://github.com/koajs/koa/issues/1466
     // We can probably remove it once jest fixes https://github.com/facebook/jest/issues/2549.
-    const isNativeError = Object.prototype.toString.call(err) === '[object Error]' || err instanceof Error
-    if (!isNativeError) throw new TypeError(util.format('non-error thrown: %j', err))
+    const isNativeError =
+      Object.prototype.toString.call(err) === "[object Error]" ||
+      err instanceof Error;
+    if (!isNativeError)
+      throw new TypeError(util.format("non-error thrown: %j", err));
 
-    if (err.status === 404 || err.expose) return
-    if (this.silent) return
+    if (err.status === 404 || err.expose) return;
+    if (this.silent) return;
 
-    const msg = err.stack || err.toString()
-    console.error(`\n${msg.replace(/^/gm, '  ')}\n`)
+    const msg = err.stack || err.toString();
+    console.error(`\n${msg.replace(/^/gm, "  ")}\n`);
   }
 
   /**
@@ -218,9 +315,9 @@ module.exports = class Application extends Emitter {
    */
 
   static get default() {
-    return Application
+    return Application;
   }
-}
+};
 
 /**
  * Response helper.
@@ -228,60 +325,60 @@ module.exports = class Application extends Emitter {
 
 function respond(ctx) {
   // allow bypassing koa
-  if (ctx.respond === false) return
+  if (ctx.respond === false) return;
 
-  if (!ctx.writable) return
+  if (!ctx.writable) return;
 
-  const res = ctx.res
-  let body = ctx.body
-  const code = ctx.status
+  const res = ctx.res;
+  let body = ctx.body;
+  const code = ctx.status;
 
   // ignore body
   if (statuses.empty[code]) {
     // strip headers
-    ctx.body = null
-    return res.end()
+    ctx.body = null;
+    return res.end();
   }
 
-  if (ctx.method === 'HEAD') {
-    if (!res.headersSent && !ctx.response.has('Content-Length')) {
-      const { length } = ctx.response
-      if (Number.isInteger(length)) ctx.length = length
+  if (ctx.method === "HEAD") {
+    if (!res.headersSent && !ctx.response.has("Content-Length")) {
+      const { length } = ctx.response;
+      if (Number.isInteger(length)) ctx.length = length;
     }
-    return res.end()
+    return res.end();
   }
 
   // status body
   if (body == null) {
     if (ctx.response._explicitNullBody) {
-      ctx.response.remove('Content-Type')
-      ctx.response.remove('Transfer-Encoding')
-      ctx.length = 0
-      return res.end()
+      ctx.response.remove("Content-Type");
+      ctx.response.remove("Transfer-Encoding");
+      ctx.length = 0;
+      return res.end();
     }
     if (ctx.req.httpVersionMajor >= 2) {
-      body = String(code)
+      body = String(code);
     } else {
-      body = ctx.message || String(code)
+      body = ctx.message || String(code);
     }
     if (!res.headersSent) {
-      ctx.type = 'text'
-      ctx.length = Buffer.byteLength(body)
+      ctx.type = "text";
+      ctx.length = Buffer.byteLength(body);
     }
-    return res.end(body)
+    return res.end(body);
   }
 
   // responses
-  if (Buffer.isBuffer(body)) return res.end(body)
-  if (typeof body === 'string') return res.end(body)
-  if (body instanceof Stream) return body.pipe(res)
+  if (Buffer.isBuffer(body)) return res.end(body);
+  if (typeof body === "string") return res.end(body);
+  if (body instanceof Stream) return body.pipe(res);
 
   // body: json
-  body = JSON.stringify(body)
+  body = JSON.stringify(body);
   if (!res.headersSent) {
-    ctx.length = Buffer.byteLength(body)
+    ctx.length = Buffer.byteLength(body);
   }
-  res.end(body)
+  res.end(body);
 }
 
 /**
@@ -289,4 +386,4 @@ function respond(ctx) {
  * have a direct dependency upon `http-errors`
  */
 
-module.exports.HttpError = HttpError
+module.exports.HttpError = HttpError;
--- src/SOURCE-CODE-ANALYSIS/KOA/lib/koa-compose.js
+++ src/SOURCE-CODE-ANALYSIS/KOA/lib/koa-compose.js
@@ -21,8 +21,7 @@ module.exports = compose
 // - 参数
 //    - 中间价组成的数组，每个中间件必须是函数
 // - 返回值
-//    - 返回一个函数
-//    -
+//    - 返回一个函数，该返回函数的参数是 context 和 next
 function compose (middleware) {
   if (!Array.isArray(middleware)) throw new TypeError('Middleware stack must be an array!') // 参数必须是数组
   for (const fn of middleware) {
@@ -39,6 +38,7 @@ function compose (middleware) {
     // last called middleware #
     let index = -1
     return dispatch(0)
+
     function dispatch (i) { // 调用时：dispatch(0)
 
       if (i <= index) return Promise.reject(new Error('next() called multiple times'))
@@ -52,6 +52,10 @@ function compose (middleware) {
       // 第一个中间件函数
 
       if (i === middleware.length) fn = next
+      // 当为最后一个中间件时，继续执行 dispatch() --> fn=next=undefined --> return Promise.resolve()，
+      // - next不存在，return Promise.resolve()
+      // - next存在，则执行next(context, dispatch.bind(null, i+1))
+
 
       // fn不存在
       if (!fn) return Promise.resolve()
@@ -63,9 +67,31 @@ function compose (middleware) {
         // 返回Promise成功的结果 -> 即执行中间件，中间件返回值作为promise成功状态的结果值
         // 1
         // 中间件函数的结构
-        //  app.use(async ctx => { ctx.body = 'Hello World' })
-        //  async ctx => { ctx.body = 'Hello World' }
+        // app.use(async (ctx, next) => { ... })
+        // async (ctx, next) => {...}
+        // 2
+        // fn(context, dispatch.bind(null, i + 1)) ---> next() 其实就是这里的 dispatch() 函数 ---> i+1就是下一个中间件
 
+        // 2022.04.11 添加一个问题
+        // 问题：到底是什么是中间件中的next
+        // 回答：
+        // - 1. 中间件 app.use(async (ctx, next) => { ... })
+        // - 2. next 就是上面的 dispatch 函数
+        // - 3. 而 dispatch 又会去执行下一个中间件，不断重复，直到最后一个中间件
+        // - 4. 还要注意 next() 调用的时机，如果next() 后面还有代码，在next()执行完后，还会回来继续执行
+
+        // 2023.03.01
+        // 最终形态
+        // const [fn1, fn2, fn3] = this.middleware()
+        // const fnMiddleware = function(context, next) {
+        //   return Promise.resolve(fn1(context, function next1() {
+        //     return Promise.resolve(fn2(context, function next2() { // 每个中间件，如果存在next就return Promise.resolve(fn(context, dispatch.bind(null, i + 1)))，不存在就 return Promise.resolve()
+        //       return Promise.resolve(fn3(context, function next3() { // 最后一个中间件没有next函数了，因为已经是最后一个
+        //         return Promise.resolve()
+        //       }))
+        //     }))
+        //   }))
+        // }
       } catch (err) {
         return Promise.reject(err)
       }
